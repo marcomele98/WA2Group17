a1bc0b79772802a0f2f7bfc39d48470456cc4159
frusso <fran.russo@reply.it>
2023-05-05 01:01:55 +0200

    fix: deleted unused queries

--- server/src/main/kotlin/it/polito/wa2/g17/server/ticketing/tickets/TicketRepository.kt
+++ server/src/main/kotlin/it/polito/wa2/g17/server/ticketing/tickets/TicketRepository.kt
@@ -17,16 +17,4 @@ interface TicketRepository : JpaRepository<Ticket, Long> {
     //Only for testing purposes DO NOT USE
     @Query("SELECT t FROM Ticket t JOIN FETCH t.statusHistory")
     fun findAllEager(): List<Ticket>
-
-    //Only for testing purposes DO NOT USE
-    @Query("SELECT t FROM Ticket t JOIN FETCH t.messages")
-    fun findAllWithMessages(): List<Ticket>
-
-    @Query("SELECT t FROM Ticket t JOIN FETCH t.messages WHERE t.id = :id")
-    fun findTicketWithMessages(id: Long?): Ticket
-
-    //Only for testing purposes DO NOT USE
-    @Query("SELECT m FROM Message m JOIN FETCH m.attachments")
-    fun findAllMessagesWithAttachments(): List<Message>
-
 }
