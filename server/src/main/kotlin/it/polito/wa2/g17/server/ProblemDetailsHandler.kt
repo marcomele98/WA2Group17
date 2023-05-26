2f470cc431ea86f8b2da6f15c5136c67632d5615
Marco Mele s296538 <s296538@studenti.polito.it>
2023-05-26 15:11:29 +0200

    feat: added error logs

--- server/src/main/kotlin/it/polito/wa2/g17/server/ProblemDetailsHandler.kt
+++ server/src/main/kotlin/it/polito/wa2/g17/server/ProblemDetailsHandler.kt
@@ -1,67 +1,93 @@
 package it.polito.wa2.g17.server
 
-import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.RestControllerAdvice
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import io.micrometer.observation.annotation.Observed
 import it.polito.wa2.g17.server.products.ProductNotFoundException
 import it.polito.wa2.g17.server.profiles.CannotUpdateEmailException
 import it.polito.wa2.g17.server.profiles.DuplicateProfileException
 import it.polito.wa2.g17.server.profiles.ProfileNotFoundException
 import it.polito.wa2.g17.server.ticketing.tickets.*
-import jakarta.servlet.ServletException
 import jakarta.validation.ConstraintViolationException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ProblemDetail
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @RestControllerAdvice
+@Observed
 class ProblemDetailsHandler : ResponseEntityExceptionHandler() {
+
+    private val log: Logger = LoggerFactory.getLogger(ProblemDetailsHandler::class.java)
+
     @ExceptionHandler(ProductNotFoundException::class)
-    fun handleProductNotFound(e: ProductNotFoundException) =
+    fun handleProductNotFound(e: ProductNotFoundException) {
+        log.error(e.toString())
         ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.message!!)
+    }
 
     @ExceptionHandler(DuplicateProfileException::class)
-    fun handleDuplicateProfile(e: DuplicateProfileException) =
+    fun handleDuplicateProfile(e: DuplicateProfileException) {
+        log.error(e.toString())
         ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.message!!)
+    }
 
     @ExceptionHandler(ProfileNotFoundException::class)
-    fun handleProfileNotFound(e: ProfileNotFoundException) =
+    fun handleProfileNotFound(e: ProfileNotFoundException) {
+        log.error(e.toString())
         ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.message!!)
+    }
 
     @ExceptionHandler(ConstraintViolationException::class)
     fun handleValidationException(ex: ConstraintViolationException): ProblemDetail {
         val errors = ex.constraintViolations.joinToString("; ") {
                 it.messageTemplate
         }
+        log.error(ex.toString())
         return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, errors!!)
     }
 
     @ExceptionHandler(CannotUpdateEmailException::class)
-    fun handleEmailException(e: CannotUpdateEmailException) =
+    fun handleEmailException(e: CannotUpdateEmailException) {
+        log.error(e.toString())
         ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, e.message!!)
+    }
 
     @ExceptionHandler(TicketNotFoundException::class)
-    fun handleTicketNotFound(e: TicketNotFoundException) =
+    fun handleTicketNotFound(e: TicketNotFoundException) {
+        log.error(e.toString())
         ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.message!!)
+    }
 
     @ExceptionHandler(WrongStateException::class)
-    fun handleWrongState(e: WrongStateException) =
+    fun handleWrongState(e: WrongStateException) {
+        log.error(e.toString())
         ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.message!!)
+    }
 
     @ExceptionHandler(WrongSkillsException::class)
-    fun handleWrongSkills(e: WrongSkillsException) =
+    fun handleWrongSkills(e: WrongSkillsException) {
+        log.error(e.toString())
         ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, e.message!!)
+    }
 
     @ExceptionHandler(WrongUserException::class)
-    fun handleWrongUser(e: WrongUserException) =
+    fun handleWrongUser(e: WrongUserException) {
+        log.error(e.toString())
         ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, e.message!!)
+    }
 
     @ExceptionHandler(WrongAttachmentsException::class)
-    fun handleWrongAttachments(e: WrongAttachmentsException) =
+    fun handleWrongAttachments(e: WrongAttachmentsException) {
+        log.error(e.toString())
         ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, e.message!!)
+    }
 
     @ExceptionHandler(AttachmentNotFoundException::class)
-    fun handleAttachmentsNotFound(e: AttachmentNotFoundException) =
+    fun handleAttachmentsNotFound(e: AttachmentNotFoundException) {
+        log.error(e.toString())
         ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.message!!)
-
+    }
 
 }
--- server/src/main/kotlin/it/polito/wa2/g17/server/products/ProductController.kt
+++ server/src/main/kotlin/it/polito/wa2/g17/server/products/ProductController.kt
@@ -1,30 +1,23 @@
 package it.polito.wa2.g17.server.products
 
-import io.micrometer.observation.annotation.Observed
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 
 @RestController
 @RequestMapping("/API/products")
-@Observed
 class ProductController(private val productService: ProductService) {
 
-    private val log: Logger = LoggerFactory.getLogger(ProductController::class.java)
 
     @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
     fun getAll(): List<ProductDTO>{
-        log.info("receive get all product command.");
         return productService.getAll()
     }
 
     @GetMapping("{ean}")
     @ResponseStatus(HttpStatus.OK)
     fun getProduct(@PathVariable ean: String): ProductDTO? {
-        log.info("receive get product command.")
         return productService.getProduct(ean)
     }
 }
