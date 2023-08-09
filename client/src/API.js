a50084691fb32bd273a3297875f1623efbaebd64
Federico Rinaudi <federicorinaudi@gmail.com>
2023-08-09 16:37:22 +0200

    feat: started transition to axios

--- client/src/API.js
+++ client/src/API.js
@@ -1,93 +1,92 @@
-const URL = '/API/';
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+const APIURL = new URL("http://localhost:3001/API/");
+
+axios.interceptors.response.use(null, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  if (error.response?.status === 401) {
+    
+  }
+  return Promise.reject(error);
+});
+
+/*
+// Add a 401 response interceptor
+window.axios.interceptors.response.use(function (response) {
+    return response;
+}, function (error) {
+    if (401 === error.response.status) {
+        // handle error: inform user, go to login, etc
+    } else {
+        return Promise.reject(error);
+    }
+});
+*/
+
+async function logIn(username, password) {
+  // call: POST /api/sessions
+  try {
+    let response = await axios.post(new URL("login", APIURL), {
+      username: username,
+      password: password,
+    });
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
+}
 
 async function getProducts() {
-    return new Promise((resolve, reject) => {
-        fetch(URL + 'products/')
-            .then((response) => {
-                if (response.ok) {
-                    resolve(response.json());
-                } else {
-                    response.json()
-                        .then((obj) => { reject(obj); })
-                        .catch(() => { reject({ error: "Cannot parse server response." }) });
-                }
-            }).catch(() => { reject({ error: "Cannot communicate with the server." }) });
-    });
-};
+  // call: GET /api/products
+  try {
+    let response = await axios.get("API/products");
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
+}
 
 async function getProductByEan(ean) {
-    return new Promise((resolve, reject) => {
-        fetch(URL + 'products/' + ean)
-            .then((response) => {
-                if (response.ok) {
-                    resolve(response.json());
-                } else {
-                    response.json()
-                        .then((obj) => { reject(obj); })
-                        .catch(() => { reject({ error: "Cannot parse server response." }) });
-                }
-            }).catch(() => { reject({ error: "Cannot communicate with the server." }) });
-    });
+  // call: GET /api/products/:ean
+  try {
+    let response = await axios.get("API/products/" + ean);
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
 }
 
 async function getProfileByEmail(email) {
-    return new Promise((resolve, reject) => {
-        fetch(URL + 'profiles/' + email)
-            .then((response) => {
-                if (response.ok) {
-                    resolve(response.json());
-                } else {
-                    response.json()
-                        .then((obj) => { reject(obj); })
-                        .catch(() => { reject({ error: "Cannot parse server response." }) });
-                }
-            }).catch(() => { reject({ error: "Cannot communicate with the server." }) });
-    });
+  // call: GET /api/profiles/:email
+  try {
+    let response = await axios.get("API/profiles/" + email);
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
 }
 
-
 async function createProfile(profile) {
-    return new Promise((resolve, reject) => {
-      fetch(URL + 'profiles', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(profile),
-      }).then((response) => {
-        if (response.ok) {
-          resolve(response.json());
-        } else {
-          response.json()
-            .then((obj) => { reject(obj); })
-            .catch(() => { reject({ error: "Cannot parse server response." }) });
-        }
-      }).catch(() => { reject({ error: "Cannot communicate with the server." }) }); // connection errors
-    });
+  // call: POST /api/profiles
+  try {
+    let response = await axios.post("API/profiles", profile);
+    return response.data;
+  } catch (err) {
+    throw err;
   }
+}
 
-
-  async function updateProfile(profile, email) {
-    return new Promise((resolve, reject) => {
-      fetch(URL + 'profiles/'+ email, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(profile),
-      }).then((response) => {
-        if (response.ok) {
-          resolve(null);
-        } else {
-          response.json()
-            .then((obj) => { reject(obj); }) // error message in the response body
-            .catch(() => { reject({ error: "Cannot parse server response." }) }); // something else
-        }
-      }).catch(() => { reject({ error: "Cannot communicate with the server." }) }); // connection errors
-    });
+async function updateProfile(profile, email) {
+  // call: PUT /api/profiles/:email
+  try {
+    let response = await axios.put("API/profiles/" + email, profile);
+    return response.data;
+  } catch (err) {
+    throw err;
   }
-
-
+}
 
 const API = { getProducts, getProductByEan, getProfileByEmail, createProfile, updateProfile }
 
